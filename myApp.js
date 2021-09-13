4e023638bea5ff5ec92ed636a949d681e7b845bf
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-13 13:56:13 +0530

    Get Data from POST Requests

--- myApp.js
+++ myApp.js
@@ -62,4 +62,9 @@ app.get("/name", function (req, res) {
   res.json({ name: `${firstName} ${lastName}` });
 });
 
+app.post("/name", function(req, res) {
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
+
 module.exports = app;
