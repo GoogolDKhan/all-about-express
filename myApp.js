5a75e8561a37c5afdd3f203e79dcda0c1fcdab67
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-13 13:03:06 +0530

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ console.log("Hello World");
 //     res.send("Hello Express");
 //   });
 
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/views/index.html");
+// });
+
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
