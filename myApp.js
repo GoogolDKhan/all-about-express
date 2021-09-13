bcb4f386cfb84e73cd9052614877497b6157f1eb
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-13 13:18:34 +0530

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+require("dotenv").config();
 
 console.log("Hello World");
 
@@ -16,8 +17,16 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// app.get("/json", (req, res) => {
+//   res.json({ message: "Hello json" });
+// });
+
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
-});
+    if(process.env.MESSAGE_STYLE==="uppercase") {
+      res.json({"message": "HELLO JSON"});
+    } else {
+      res.json({"message": "Hello json"});
+    }
+  });
 
 module.exports = app;
